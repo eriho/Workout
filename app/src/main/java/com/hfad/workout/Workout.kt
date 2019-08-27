b7d76a858a9be739ddfe4cf03f86a8d39bb61842
Yosuke Horie <a6988@n-koei.co.jp>
2019-08-27 17:18:59 +0900

    Initial commit

---
+++ app/src/main/java/com/hfad/workout/Workout.kt
@@ -0,0 +1,13 @@
+package com.hfad.workout
+
+class Workout(val name: String, val description : String) {
+}
+
+val workouts = arrayOf(Workout("The Limb Loosener",
+    "5 Handstand push-ups\n10 1-legged squats\n15pull-ups"),
+    Workout("Core Agony",
+        "100 pull-ups\n100 Push-ups\n100 Sit-ups\n100 Squats"),
+    Workout("The Wimp Sepcial",
+        "5 Pull-ups\n10 Push-ups\n15Squats"),
+    Workout("Strength and Length",
+        "500 meter run\n21 x 1.5 pood kettleball swing\n21 x pull-ups"))
