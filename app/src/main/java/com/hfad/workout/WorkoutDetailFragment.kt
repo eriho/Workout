3dc27ad63b37126169047ba8077b3eb26987e45b
eriho <onipochiflute1016@gmail.com>
2019-09-11 15:41:54 +0900

    workoutDetailFragmentを完成させた

--- app/src/main/java/com/hfad/workout/WorkoutDetailFragment.kt
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.kt
@@ -6,11 +6,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class WorkoutDetailFragment : Fragment() {
 
-    private lateinit var workoutId : Long
+    private var workoutId : Long = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,6 +20,22 @@ class WorkoutDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false)
     }
+
+    override fun onStart(){
+        super.onStart()
+        val view : View? = view
+
+        if (view != null) {
+
+            val title: TextView = view.findViewById(R.id.textTitle) as TextView
+            val workout: Workout = workouts[workoutId.toInt()]
+            title.text = workout.name
+            val description: TextView = view.findViewById(R.id.textDescription) as TextView
+            description.text = workout.description
+        }
+
+    }
+
 
     fun setWorkout(id:Long){
         this.workoutId = id
